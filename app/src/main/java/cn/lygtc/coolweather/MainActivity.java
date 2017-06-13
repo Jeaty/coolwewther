57e7dd1a322ddf3ffc42db64d246ef4ac44e79dc
Jeaty <1901937291@qq.com>
2017-06-13 11:28:58 +0800

    gkigik

--- app/src/main/java/cn/lygtc/coolweather/MainActivity.java
+++ app/src/main/java/cn/lygtc/coolweather/MainActivity.java
@@ -3,7 +3,10 @@ package cn.lygtc.coolweather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.baidu.location.LocationClient;
+
 public class MainActivity extends AppCompatActivity {
+    private LocationClient locationClient ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
